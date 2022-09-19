281ed1b46e8df2b5275099624ebd5d41ccc01903
udenr <anders@vandret.dk>
2022-09-19 19:57:13 +0200

    Add project files.

---
+++ Transportberegner/Classes/Beregner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportberegner
+{
+    public class IBeregner
+    {
+        public int BeregnTransportpris(int afstand, int vaegt)
+        {
+            int transportpris;
+            if (afstand < 5 && vaegt < 10)
+            {
+                transportpris = 0;
+            }
+            else if (afstand < 5 && vaegt >= 10)
+            {
+                transportpris = 50;
+            }
+            else if (afstand >= 5 && vaegt < 10)
+            {
+                transportpris = 75;
+            }
+            else
+            {
+                transportpris = 100;
+            }
+            return transportpris;
+        }
+    }
+}
---
+++ Transportberegner/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Transportberegner;
+
+namespace Transportberegner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Transportprisberegner");
+            Console.Write("Indtast afstanden til destinationen (km): ");
+            int afstandTilDestinationen = Int32.Parse(Console.ReadLine());
+            Console.Write("Indtast vægt (kg): ");
+            int varensVaegt = Int32.Parse(Console.ReadLine());
+
+            var transportprisObject = new IBeregner();
+
+            int transportpris = transportprisObject.BeregnTransportpris(afstandTilDestinationen, varensVaegt);
+
+            Console.WriteLine("Prisen er "+transportpris+" kr.");
+            Console.ReadLine();
+        }
+
+        
+    }
+}
